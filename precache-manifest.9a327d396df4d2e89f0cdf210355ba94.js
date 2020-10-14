ae723c38b7763ce96db2db9a41c65f801085ba2c
Mandar08 <70933687+Mandar08@users.noreply.github.com>
2020-10-14 18:29:51 +0530

    Add files via upload

---
+++ precache-manifest.9a327d396df4d2e89f0cdf210355ba94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b6dc5a52802fe4eb084e1b58ec75fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15594740b4f27926154f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15594740b4f27926154f",
+    "url": "/static/js/main.d639548b.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
